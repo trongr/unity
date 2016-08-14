035a55de23c560a21a4d24bca76b3170f094495b
trongr <devgruxcon@gmail.com>
2016-08-13 21:32:58 -0400

    rolling ball

---
+++ Roll a Ball/Assets/scripts/PlayerCtrl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCtrl : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate() {
+        float moveHor = Input.GetAxis("Horizontal");
+        float moveVer = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHor, 0, moveVer);
+        rb.AddForce(movement * speed);
+    }
+}
