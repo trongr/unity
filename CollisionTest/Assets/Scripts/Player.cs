9bd32f070b576ed90f3189268e06041d8d253002
trong <devgruxcon@gmail.com>
2018-06-16 17:00:25 -0400

    Collision: rigidbody

---
+++ CollisionTest/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 6;
+	Vector3 velocity;
+	Rigidbody playerrb;
+
+	// Use this for initialization
+	void Start() {
+		playerrb = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+		Vector3 direction = input.normalized;
+		velocity = direction * speed;
+	}
+
+	// Use this for physics updates.
+	void FixedUpdate() {
+		playerrb.position += velocity * Time.fixedDeltaTime;
+	}
+}
