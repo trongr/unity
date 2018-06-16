6685fb6563ca02b9c011402fed617ed164592d39
trong <devgruxcon@gmail.com>
2018-06-16 02:27:18 -0400

    Unity: learning projects

---
+++ MoveAndFollowTest/Assets/Scripts/Follower.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour {
+
+	public Transform player;
+	public float speed = 7f;
+
+	// Update is called once per frame
+	void Update() {
+		Vector3 displacementFromPlayer = player.position - transform.position;
+		Vector3 directionToPlayer = displacementFromPlayer.normalized;
+		Vector3 velocity = directionToPlayer * speed;
+		Vector3 displacement = velocity * Time.deltaTime;
+		if (displacementFromPlayer.magnitude > 2) {
+			transform.Translate(displacement);
+		}
+	}
+}
---
+++ MoveAndFollowTest/Assets/Scripts/Player.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 10f;
+
+	// Update is called once per frame
+	void Update() {
+		Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+		Vector3 direction = input.normalized;
+		Vector3 velocity = direction * speed;
+		Vector3 displacement = velocity * Time.deltaTime;
+		transform.Translate(displacement);
+	}
+}
